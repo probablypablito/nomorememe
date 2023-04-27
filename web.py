f6dc7034d040ed439a02371151b7125d62cf2b22
pablito <55856800+probablypablito@users.noreply.github.com>
2023-04-26 21:01:45 -0400

    Add a homepage

--- web.py
+++ web.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def serve_image():
     query = request.args.get('image')
+    if (query == None): return render_template('index.html')
     img_buffer = getImage(query)
     meme = generate_meme(img_buffer, "NO MORE", query.upper().replace("_", " "))
     return send_file(BytesIO(meme), mimetype='image/jpeg')
